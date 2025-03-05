9ce752609cdee52e7266b98991bb288596a2940b
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-05 21:49:11 +0800

    Implemented forum_list() and forum_detail() views

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Post, PostCategory
 
-# Create your views here.
+
+def forum_list(request):
+    ctx = {
+        "posts": Post.objects.all(),
+        "categories": PostCategory.objects.all(),
+    }
+    return render(request, "forum/forum_list.html", ctx)
+
+
+def forum_detail(request, slug):
+    ctx = {'post': Post.objects.get(slug=slug)}
+    return render(request, 'forum/forum_detail.html', ctx)
