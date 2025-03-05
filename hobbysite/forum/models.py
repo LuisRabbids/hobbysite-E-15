e0feb13524de2b0d2d9df91b9dbbe010d1352e5c
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-05 21:15:57 +0800

    Implemented the Post() model for forum

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -7,3 +8,18 @@ class PostCategory(models.Model):
 
     class Meta:
         ordering = ['name']  # categories sorted by name in ascending order
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        PostCategory,
+        on_delete = models.SET_NULL,
+        null = True
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
