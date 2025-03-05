994639e2da000442c1406a154d1d82ba46346c5d
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-05 21:05:26 +0800

    Implemented the PostCategory() model

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']  # categories sorted by name in ascending order
