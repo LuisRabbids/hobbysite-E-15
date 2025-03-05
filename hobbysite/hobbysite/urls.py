9da3c5903f503e24a3fb9dd4b1d611e89ef454b4
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-05 21:43:02 +0800

    Fixed 'forum.urls' typo in hobbysite urls

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("forum/", include("tasks,urls")),
+    path("forum/", include("forum.urls")),
     path('admin/', admin.site.urls),
 ]
