8100b22bf391026c000031d26c65d7f5e0de6bc2
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-05 21:24:08 +0800

    Added methods to the forum's models

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
 class PostCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         ordering = ['name']  # categories sorted by name in ascending order
@@ -14,12 +18,18 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     category = models.ForeignKey(
         PostCategory,
-        on_delete = models.SET_NULL,
-        null = True
+        on_delete=models.SET_NULL,
+        null=True
     )
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
+    def get_slug(self):
+        return slugify(self.title)
+
     class Meta:
-        ordering = ['-created_on']
+        ordering = ['-created_on']  # posts sorted by date it was created, in descending order
