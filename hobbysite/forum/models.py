9b8afed67e161ac4a83d26844a2de4aae324edd8
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-07 01:46:03 +0800

    All forum app specs are completed

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.text import slugify
 
 
 class PostCategory(models.Model):
@@ -23,14 +22,6 @@ class Post(models.Model):
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.title
     
     def __str__(self):
         return f"{self.title} ({self.category.name if self.category else 'No Category'})"
