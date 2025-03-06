676c8d235a169d5a96aad616c75c8fb594e3e9af
Luis Felipe <luisfelipecodes@gmail.com>
2025-03-06 14:22:37 +0800

    Changed url path of forum list to follow specs

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import forum_list, forum_detail
 
 urlpatterns = [
-    path('', forum_list, name="forum-list"),
+    path('threads/', forum_list, name="forum-list"),
     path('<slug:slug>/', forum_detail, name='forum-detail')
 ]
 
