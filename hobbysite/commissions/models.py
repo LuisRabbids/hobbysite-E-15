a148d3ded3579ba8cdcdfbf3b72f825e55bbd3c2
PiXL <axelpagsi601@gmail.com>
2025-03-07 09:55:33 +0800

    Models added

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class CommissionCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ['name']  # SOrted by category name in ascending order
+
+    def __str__(self):
+        return self.name
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ArticleCategory,
+        null=True,
+        on_delete=models.SET_NULL)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)  # Only set on creation
+
+    class Meta:
+        ordering = ['-created_on']  # Sorted by commission creation date in descending order
+
+    def __str__(self):
+        return self.title
