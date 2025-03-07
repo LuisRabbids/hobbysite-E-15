7972f4eecf1298e78a636d833020dddac9b0f2b5
PiXL <axelpagsi601@gmail.com>
2025-03-07 23:10:33 +0800

    Final (hopefully) edits. Should be okay na

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Commission, CommissionCategory
+from .models import Commission, Comment
 
 class CommissionAdmin(admin.ModelAdmin):
-    list_display = ('title','description', 'people_required', 'created_on', 'last_updated')
+    list_display = ('title','description', 'people_required', 'created_on', 'updated_on')
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('commission', 'entry', 'created_on', 'updated_on')
--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)  # Only set on creation
+    updated_on = models.DateTimeField(auto_now=True) # Updates with modification date
+
+    class Meta:
+        ordering = ['created_on']  # Sorted by commission creation date in ascending order
+
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     commission = models.ForeignKey(
         Commission,
@@ -14,16 +27,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.name
-
-class Commission(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    people_required = models.IntegerField()
-    created_on = models.DateTimeField(auto_now_add=True)  # Only set on creation
-    updated_on = models.DateTimeField(auto_now=True) # Updates with modification date
-
-    class Meta:
-        ordering = ['created_on']  # Sorted by commission creation date in ascending order
-
-    def __str__(self):
-        return self.title
