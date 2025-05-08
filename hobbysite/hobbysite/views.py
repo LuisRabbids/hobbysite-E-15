bccc7b137dcb443b26631860ccab8713f0846ca7
John Sotelo <sotelo@Johns-MacBook-Air-2.local>
2025-05-08 17:37:53 +0800

    Added a registration view in hobbysite/views.py

--- hobbysite/hobbysite/views.py
+++ hobbysite/hobbysite/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth.forms import UserCreationForm # For basic registration
 from django.shortcuts import redirect # For registration
 from django.contrib import messages # For registration messages
 
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm # Import your new form
+from django.contrib import messages
+
 def homepage(request):
     # You can add context here if needed later, e.g., links to apps
     context = {} # Add any data you want to pass to the template here
@@ -20,3 +24,20 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+
+def homepage(request):
+      # ... (existing homepage view) ...
+      return render(request, 'homepage.html')
+
+def register(request):
+      if request.method == 'POST':
+          form = RegistrationForm(request.POST)
+          if form.is_valid():
+              form.save()
+              username = form.cleaned_data.get('username')
+              messages.success(request, f'Account created for {username}! You can now log in.')
+              return redirect('login') # Redirect to login page after successful registration
+      else:
+          form = RegistrationForm()
+      return render(request, 'registration/register.html', {'form': form})
