d77afe29af67695c67382645c732a33051d9bbbd
John Sotelo <sotelo@Johns-MacBook-Air-2.local>
2025-05-08 16:57:58 +0800

    updated admin

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,6 +1,27 @@
+# wiki/admin.py
 from django.contrib import admin
-from .models import ArticleCategory, Article
+from .models import ArticleCategory, Article, Comment # Add Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author', 'created_on', 'updated_on')
+    list_filter = ('category', 'author', 'created_on')
+    search_fields = ('title', 'entry')
+    readonly_fields = ('created_on', 'updated_on') # Good to show but not edit here
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('article', 'author', 'created_on', 'entry_short')
+    list_filter = ('author', 'created_on')
+    search_fields = ('entry', 'author__username', 'article__title')
+
+    def entry_short(self, obj): # Display a snippet of the comment
+        return obj.entry[:75] + '...' if len(obj.entry) > 75 else obj.entry
+    entry_short.short_description = 'Comment Snippet'
 
 
-admin.site.register(ArticleCategory)
-admin.site.register(Article)
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
