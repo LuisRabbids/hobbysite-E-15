7b343663f94b3c1260070b7edbd2187d6b6addb4
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-12 11:23:14 +0800

    Added a bio field for the Profile() class

--- hobbysite/accounts/models.py
+++ hobbysite/accounts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
 
 
 class TaskGroup(models.Model):
