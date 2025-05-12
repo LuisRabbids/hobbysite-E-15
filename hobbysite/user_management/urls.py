5b9e2f0d377401e8778d2e486a6a1098a5df8c7a
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-12 12:36:29 +0800

    Updated update_profile() view in user_management

---
+++ hobbysite/user_management/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import update_profile
+
+urlpatterns = [
+    path('profile/update/', update_profile, name='update_profile'),
+]
+
+
+app_name = 'user_management'
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from .models import Profile
 from .forms import ProfileUpdateForm
 
 
 @login_required
-def update_profile(request):
-    profile = request.user.profile
+def update_profile(request, display_name):
+    profile = get_object_or_404(Profile, display_name=display_name)
+
+    if profile.user != request.user:
+        return HttpResponseForbidden("You are not allowed to edit this profile.")
 
     if request.method == 'POST':
         form = ProfileUpdateForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            form = ProfileUpdateForm(instance=profile)  # re-render with updated data
+            # reload with saved data
+            form = ProfileUpdateForm(instance=profile)
     else:
         form = ProfileUpdateForm(instance=profile)
 
