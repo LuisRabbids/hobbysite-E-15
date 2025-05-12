30ad242d6044ece0194663e9ad3f8c06e358d96b
01111000x <jancel.ensina@gmail.com>
2025-05-13 02:55:44 +0800

    added new paths in urls

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from . import views
 
 app_name = 'merchstore'
 
 urlpatterns = [
-    path('items/', ProductListView.as_view(), name='product-list'),
-    path('item/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('items/', views.ProductListView.as_view(), name='product-list'),
+    path('item/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('item/add/', views.ProductCreateView.as_view(), name='product-add'),
+    path('item/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='product-edit'),
+    path('cart/', views.CartView.as_view(), name='cart'),
+    path('transactions/', views.SellerTransactionListView.as_view(), name='transactions'),
 ]
