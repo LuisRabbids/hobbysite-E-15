5b4ea658c94724976cd072ff12d2739a5a4bd421
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-12 11:55:51 +0800

    Removed models from accounts models.py

--- hobbysite/accounts/models.py
+++ hobbysite/accounts/models.py
@@ -1,11 +1,2 @@
 from django.contrib.auth.models import User
 from django.db import models
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-
-
-class TaskGroup(models.Model):
-    name = models.CharField(max_length=50)
