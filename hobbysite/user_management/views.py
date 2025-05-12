dc4539a35a4f22368fba18bd8183dd239e9cea48
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-12 12:31:10 +0800

    Added update_profile() view in user_management

--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm
 
-# Create your views here.
+
+@login_required
+def update_profile(request):
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            form = ProfileUpdateForm(instance=profile)  # re-render with updated data
+    else:
+        form = ProfileUpdateForm(instance=profile)
+
+    return render(request, 'user_management/update_profile.html', {'form': form})
