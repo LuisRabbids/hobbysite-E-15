af1fa054784abd4940752c69a92cf1089deab037
01111000x <jancel.ensina@gmail.com>
2025-05-13 10:51:52 +0800

    rewrote merchstore urls

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'merchstore'
 
 urlpatterns = [
-    path('items/', views.ProductListView.as_view(), name='product-list'),
-    path('item/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
-    path('item/add/', views.ProductCreateView.as_view(), name='product-add'),
-    path('item/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='product-edit'),
-    path('cart/', views.CartView.as_view(), name='cart'),
-    path('transactions/', views.SellerTransactionListView.as_view(), name='transactions'),
+    path('items/', views.product_list, name='product-list'),
+    path('item/<int:pk>/', views.product_detail, name='product-detail'),
+    path('item/add/', views.product_create, name='product-add'),
+    path('item/<int:pk>/edit/', views.product_edit, name='product-edit'),
+    path('cart/', views.cart, name='cart'),
+    path('transactions/', views.transactions, name='transactions'),
 ]
