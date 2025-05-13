25e792b5c9ad2b75ec36fe3502d171f130e9efc1
01111000x <jancel.ensina@gmail.com>
2025-05-13 09:56:15 +0800

    modified merchstore/admin

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
-from .models import ProductType, Product
+from .models import ProductType, Product, Transaction
 
 @admin.register(ProductType)
 class ProductTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = (
+        'name',
+        'description'
+    )
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'product_type', 'price')
-    list_filter = ('product_type',)
-    search_fields = ('name', 'description')
+    list_display = (
+        'name',
+        'product_type',
+        'owner',
+        'price',
+        'stock',
+        'status'
+    )
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'buyer',
+        'amount',
+        'status',
+        'created_on'
+    )
