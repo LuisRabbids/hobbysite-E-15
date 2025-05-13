3d072a41e61c659bdb3df93e17198e9fd1b923d4
01111000x <jancel.ensina@gmail.com>
2025-05-13 10:49:52 +0800

    modified merchstore views to be function-based

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,70 +1,86 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.shortcuts import redirect
-from .models import Product, Transaction
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Transaction, ProductType
+from .forms import ProductForm, TransactionForm
+from user_management.models import Profile
 
-class ProductListView(ListView):
-    model = Product
-    def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        if self.request.user.is_authenticated:
-            profile = self.request.user.profile
-            user_items = Product.objects.filter(owner=profile)
-            all_items = Product.objects.exclude(owner=profile)
-            ctx.update({'user_items':user_items,'object_list':all_items})
-        return ctx
+User = get_user_model()
 
-class ProductDetailView(DetailView):
-    model = Product
-    def post(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect('login')
-        self.object = self.get_object()
-        form_amount = int(request.POST['amount'])
-        if self.object.owner == request.user.profile or self.object.stock < form_amount:
-            return self.get(request,*args,**kwargs)
-        Transaction.objects.create(
-          buyer=request.user.profile,
-          product=self.object,
-          amount=form_amount
-        )
-        self.object.stock -= form_amount
-        self.object.save()
-        return redirect('merchstore:cart')
+@login_required
+def product_list(request):
+    all_products = Product.objects.select_related('product_type').all()
+    user_profile = request.user.profile
+    user_products = all_products.filter(owner=user_profile)
+    other_products = all_products.exclude(owner=user_profile)
 
-class ProductCreateView(LoginRequiredMixin, CreateView):
-    model = Product
-    fields = [
-        'name',
-        'product_type',
-        'description',
-        'price',
-        'stock',
-        'status'
-    ]
-    def form_valid(self, form):
-        form.instance.owner = self.request.user.profile
-        return super().form_valid(form)
+    context = {
+        'user_products': user_products,
+        'other_products': other_products,
+    }
+    return render(request, 'merchstore/product_list.html', context)
 
-class ProductUpdateView(LoginRequiredMixin, UpdateView):
-    model = Product
-    fields = [
-        'name',
-        'product_type',
-        'description',
-        'price',
-        'stock',
-        'status'
-    ]
+@login_required
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    can_buy = (product.owner != request.user.profile and product.stock > 0)
+    form = TransactionForm()
 
-class CartView(LoginRequiredMixin, ListView):
-    model = Transaction
-    template_name='merchstore/cart.html'
-    def get_queryset(self):
-        return Transaction.objects.filter(buyer=self.request.user.profile)
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid() and can_buy:
+            trans = form.save(commit=False)
+            trans.buyer = request.user.profile
+            trans.product = product
+            trans.save()
+            product.stock -= trans.amount
+            product.save()
+            return redirect('merchstore:cart')
 
-class SellerTransactionListView(LoginRequiredMixin, ListView):
-    model = Transaction
-    template_name='merchstore/transactions.html'
-    def get_queryset(self):
-        return Transaction.objects.filter(product__owner=self.request.user.profile)
+    context = {
+        'product': product,
+        'form': form,
+        'can_buy': can_buy,
+    }
+    return render(request, 'merchstore/product_detail.html', context)
+
+@login_required
+def product_create(request):
+    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            prod = form.save(commit=False)
+            prod.owner = request.user.profile
+            prod.save()
+            return redirect('merchstore:product-detail', pk=prod.pk)
+
+    context = {'form': form}
+    return render(request, 'merchstore/product_form.html', context)
+
+@login_required
+def product_edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if product.owner != request.user.profile:
+        return redirect('merchstore:product-detail', pk=pk)
+
+    form = ProductForm(instance=product)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            prod = form.save()
+            return redirect('merchstore:product-detail', pk=prod.pk)
+
+    context = {'form': form, 'product': product}
+    return render(request, 'merchstore/product_form.html', context)
+
+@login_required
+def cart(request):
+    items = Transaction.objects.filter(buyer=request.user.profile, status='on_cart')
+    context = {'transactions': items}
+    return render(request, 'merchstore/cart.html', context)
+
+@login_required
+def transactions(request):
+    items = Transaction.objects.filter(product__owner=request.user.profile).exclude(status='on_cart')
+    context = {'transactions': items}
+    return render(request, 'merchstore/transactions.html', context)
