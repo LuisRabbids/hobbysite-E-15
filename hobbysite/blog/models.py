259c00896dab635b9afa9346a201806908d29dc7
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-13 10:31:10 +0800

    Fixed Profile() conflict in blog models

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
-# Create your models here.
-
-
-# Author model
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    author = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.author
+from user_management.models import Profile
 
 
 
