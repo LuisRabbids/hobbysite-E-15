bf18b17d3de523940e796b2cf837db98f38ea815
01111000x <jancel.ensina@gmail.com>
2025-05-13 09:47:26 +0800

    added merchstore form

---
+++ hobbysite/merchstore/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Transaction, Product
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount']
+        widgets = {
+            'amount': forms.NumberInput(attrs={'min': '1'})
+        }
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'product_type', 'description', 'price', 'stock', 'status']
