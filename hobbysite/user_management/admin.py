1d033b02e8f1c8aebdb86d47feadffccd3f2b1e1
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-14 21:30:56 +0800

    Fixed registration and profile class connection

--- hobbysite/user_management/admin.py
+++ hobbysite/user_management/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import Profile
 
 
+@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'display_name', 'email')  # Display fields in the list view
-    fields = ('user', 'display_name', 'email')  # Fields to show in the form view
+    list_display = ('user', 'display_name', 'get_email')
 
-
-admin.site.register(Profile, ProfileAdmin)
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
