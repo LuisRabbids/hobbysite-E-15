19be3220028a52af0366ca814e201078dfcc7625
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-15 18:49:05 +0800

    Modified fields for commissions forms

--- hobbysite/commissions/forms.py
+++ hobbysite/commissions/forms.py
@@ -4,9 +4,9 @@ from .models import Commission, Comment, Job, JobApplication
 class CommissionForm(forms.ModelForm):
     class Meta:
         model = Commission
-        fields = ['title', 'author', 'description', 'status', 'people_required', 'created_on', 'updated_on']
+        fields = ['title', 'author', 'description', 'status', 'people_required']
     
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['commission', 'entry', 'created_on', 'updated_on']
+        fields = ['commission', 'entry']
