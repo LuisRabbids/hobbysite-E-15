0d29210665260ed59fe2c9ddaa749ddced8a4713
PiXL <axelpagsi601@gmail.com>
2025-05-15 23:00:48 +0800

    Lets go?

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('list/', views.commissions_list, name='commissions_list'),
     path('detail/<int:pk>/', views.commission_detail, name='commission_detail'),
     path('add/', views.commission_create, name='commission-create'),
-    path('<int:pk>/edit/', views.commission_update, name='commission-update')
+    path('<int:pk>/edit/', views.commission_update, name='commission-update'),
 ]
