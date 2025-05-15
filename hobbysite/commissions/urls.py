a967e7310de566fc6755d98c843662d9b1f72d1a
PiXL <axelpagsi601@gmail.com>
2025-05-15 19:05:57 +0800

    HTMLs added, url edited

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import views
 
 urlpatterns = [
-    path('/commissions/list', views.commissions_list, name='commissions_list')
-    path('/commissions/detail/1', views.commission_detail, name='commission_detail')
-    path('/commissions/create', views.commission_create, name='commission-create')
-    path('/commissions/update', views.commission_update, name='commission-update')
+    path('/list', views.commissions_list, name='commissions_list')
+    path('/<int:pk>/detail', views.commission_detail, name='commission_detail')
+    path('/create', views.commission_create, name='commission-create')
+    path('/<int:pk>/update', views.commission_update, name='commission-update')
 ]
