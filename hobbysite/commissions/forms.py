caf873af898fdfbfb8dce0123589af8961ee85e7
PiXL <axelpagsi601@gmail.com>
2025-05-15 17:23:06 +0800

    Forms finalized

--- hobbysite/commissions/forms.py
+++ hobbysite/commissions/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from .models import Commission, Comment, Job, JobApplication
 
-class CommissionForm(forms.Form):
+class CommissionForm(forms.ModelForm):
     class Meta:
         model = Commission()
         fields = ['title', 'author', 'description', 'status', 'people_required', 'created_on', 'updated_on']
     
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment()
+        fields = ['commission', 'entry', 'created_on', 'updated_on']
