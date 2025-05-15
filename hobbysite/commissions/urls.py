b771e227d4e66e81d73962983463abcc3a11854a
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-15 19:13:11 +0800

    Fixed commissions urls

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/list', views.commissions_list, name='commissions_list')
-    path('/detail/<int:pk>', views.commission_detail, name='commission_detail')
-    path('/add', views.commission_create, name='commission-create')
-    path('/<int:pk>/edit', views.commission_update, name='commission-update')
+    path('list/', views.commissions_list, name='commissions_list'),
+    path('detail/<int:pk>/', views.commission_detail, name='commission_detail'),
+    path('add/', views.commission_create, name='commission-create'),
+    path('<int:pk>/edit/', views.commission_update, name='commission-update')
 ]
