0e6026ee44a012fbe9f11e3d3538c0efcf6b73df
Luis Felipe <luisfelipecodes@gmail.com>
2025-05-15 18:47:28 +0800

    Fixed commissions forms parentheses typo

--- hobbysite/commissions/forms.py
+++ hobbysite/commissions/forms.py
@@ -3,10 +3,10 @@ from .models import Commission, Comment, Job, JobApplication
 
 class CommissionForm(forms.ModelForm):
     class Meta:
-        model = Commission()
+        model = Commission
         fields = ['title', 'author', 'description', 'status', 'people_required', 'created_on', 'updated_on']
     
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = Comment()
+        model = Comment
         fields = ['commission', 'entry', 'created_on', 'updated_on']
